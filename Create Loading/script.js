361df3bb04509649806a789c7f7f9d05a352dd4e
mitesh129 <dev.mitesh.01@gmail.com>
2024-12-03 08:36:21 -0500

    directory

--- Create Loading/script.js
+++
@@ -1,7 +0,0 @@
-const load = document.querySelector('.loading')
-
-window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        load.style.display = "none";
-    }, 2000);
-})
